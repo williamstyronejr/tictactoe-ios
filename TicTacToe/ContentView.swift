fd58cf1a16c6876c0cde93733e84754179df36de
williamstyronejr <itenory@gmail.com>
2023-07-26 12:47:24 -0400

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Tyrone Williams on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Tyrone Williams on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
